53778d960ee673feb524d3b183183b8a0b85d027
Ruslan Novikov <ruslan.velcom@gmail.com>
2022-11-09 09:47:44 +0300

    First

---
+++ Video_Java/src/FileProgramms/FileEx.java
@@ -0,0 +1,14 @@
+package FileProgramms;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class FileEx {
+    public static void main(String[] args) throws FileNotFoundException {
+    File file = new File("out.txt");
+    PrintWriter writer = new PrintWriter(file);
+    writer.println("Hello");
+    writer.println("Man");
+    writer.close();
+    }
+}
